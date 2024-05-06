cb92e9fc6fe25f874bb81ae0a9f565172714de3f
D-V-GITHUB <voltagaming2001@gmail.com>
2024-05-06 15:44:14 +0300

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { NavBar } from '@/components/Navbar'
+import { NextUIProvider } from '@nextui-org/react'
 
-export default function Home() {
-  return (
-    <main>
-      
-    </main>
-  );
+export default function Home({ children }: { children: React.ReactNode }) {
+    return (
+        <NextUIProvider>
+            <main>
+                <NavBar />
+                {children}
+            </main>
+        </NextUIProvider>
+    )
 }
